46bd146a2c89be58fb08a663e9a867d367a4faed
GregMagnin <g.magnin@codeur.online>
2022-06-22 12:02:11 +0200

    ajout d'images dans le carousel, opacitée modifé, texte de test sur l'image

--- site-web/index.php
+++ site-web/index.php
@@ -6,11 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AnyWhereWeb</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body> 
 <?php include 'parties/nav.php' ?>
 <script src="js/bootstrap.bundle.min.js"></script>
-<?php include 'parties/carousel.php' ?>
+<?php include 'parties/carousel.php' ?> 
+
 </body>
 </html>
--- site-web/parties/nav.php
+++ site-web/parties/nav.php
@@ -4,22 +4,18 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-      <a class="navbar-brand" href="#">Hidden brand</a>
+      <a class="navbar-brand" href="#">AnyWhereWeb</a>
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="#">Accueil</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">Réalisations</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
+          <a class="nav-link" href="#">Nous Contacter</a>
         </li>
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
     </div>
   </div>
 </nav>
